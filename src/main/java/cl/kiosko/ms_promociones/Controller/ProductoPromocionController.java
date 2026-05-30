fce5baa044ed6118a1682b76d0a7dfeb81d5d064
erick <er.rosales@duocuc.cl>
2026-05-30 19:54:35 -0400

    Se debe terminar SWAGGER con los @ApiRespones | Se debe implementar HATEOAS

--- src/main/java/cl/kiosko/ms_promociones/Controller/ProductoPromocionController.java
+++ src/main/java/cl/kiosko/ms_promociones/Controller/ProductoPromocionController.java
@@ -3,6 +3,8 @@ package cl.kiosko.ms_promociones.Controller;
 import cl.kiosko.ms_promociones.DTO.ProductoPromocionRequestDTO;
 import cl.kiosko.ms_promociones.DTO.ProductoPromocionResponseDTO;
 import cl.kiosko.ms_promociones.Service.ProductoPromocionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,15 +13,14 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/producto_promocion")
+@Tag(name="Producto Promocion", description = "Operaciones relacionadas con el objeto/entidad de Producto Promoción, el cual solo sirve para aplicar una promoción a un producto")
 public class ProductoPromocionController {
 
     @Autowired
     private ProductoPromocionService productoPromocionService;
 
-    /**
-     * Endpoint para asociar un nuevo producto a una promoción existente
-     */
     @PostMapping("/promocion/{promocionId}")
+    @Operation(summary = "Asignar Promoción a un Producto", description = "Toma una promoción existente (Requerido por el modelo de la relación) y le asigna un producto para que este quede en oferta (Crea un objeto de ProductoPromoción)")
     public ResponseEntity<ProductoPromocionResponseDTO> asignarProducto(
             @PathVariable Long promocionId,
             @Valid @RequestBody ProductoPromocionRequestDTO dto) {
@@ -29,11 +30,8 @@ public class ProductoPromocionController {
         );
     }
 
-    /**
-     * Endpoint para eliminar un producto específico de una promoción
-     * @param id El ID de la relación en la tabla intermedia
-     */
     @DeleteMapping("/{id}")
+    @Operation(summary = "Eliminar Promoción a un Producto Por ID", description = "Se introducir el ID del ProductoPromoción a borrar. Toma una promoción existente (Requerido por el modelo de la relación) y le elimina la oferta (Elimina un objeto de ProductoPromoción)")
     public ResponseEntity<Void> eliminarRelacion(@PathVariable Long id) {
         productoPromocionService.eliminarProductoDePromocion(id);
         return ResponseEntity.noContent().build();
--- src/main/java/cl/kiosko/ms_promociones/Controller/PromocionController.java
+++ src/main/java/cl/kiosko/ms_promociones/Controller/PromocionController.java
@@ -3,6 +3,8 @@ package cl.kiosko.ms_promociones.Controller;
 import cl.kiosko.ms_promociones.DTO.PromocionRequestDTO;
 import cl.kiosko.ms_promociones.DTO.PromocionResponseDTO;
 import cl.kiosko.ms_promociones.Service.PromocionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,28 +15,33 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/promociones")
+@Tag(name="Promociones", description = "Operaciones relacionadas con el manejo de las promociones")
 public class PromocionController {
 
     @Autowired
     private PromocionService promocionService;
 
     @PostMapping
+    @Operation(summary = "Crear Promoción", description = "Crea una promoción, esta debe tener una fecha durante la cual se aplique y un porcentaje de descuento")
     public ResponseEntity<PromocionResponseDTO> crearPromocion(@Valid @RequestBody PromocionRequestDTO dto) {
         // Retorna 201 Created según el estándar definido en tu informe
         return new ResponseEntity<>(promocionService.savePromocion(dto), HttpStatus.CREATED);
     }
 
     @GetMapping
+    @Operation(summary = "Listar Promociones", description = "Busca y lista todas las promociones actualmente existentes")
     public ResponseEntity<List<PromocionResponseDTO>> listarPromociones() {
         return ResponseEntity.ok(promocionService.listPromociones());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<PromocionResponseDTO> obtenerPromocion(@PathVariable Long id) {
+    @Operation(summary = "Buscar Promoción por ID", description = "Introducimos el ID de la promoción que queremos ver y lo muestra (si es que esta existe)")
+    public ResponseEntity<PromocionResponseDTO> obtenerPromocionID(@PathVariable Long id) {
         return ResponseEntity.ok(promocionService.findPromocionById(id));
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Eliminar Promoción", description = "Elimina la promoción deseada según su ID")
     public ResponseEntity<Void> eliminarPromocion(@PathVariable Long id) {
         promocionService.deletePromocion(id);
         return ResponseEntity.noContent().build();
